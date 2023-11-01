9c95d3db6d2650b6c40a7cf38a543f3ffdd6a9c7
Fedev <Fede91@users.noreply.github.com>
2023-11-01 11:42:51 +0100

    Implemented login

--- src/app/(auth)/login/components/user-auth-form.tsx
+++ src/app/(auth)/login/components/user-auth-form.tsx
@@ -7,20 +7,41 @@ import { Icons } from "@/components/icons";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useRouter } from "next/navigation";
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
   const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [password, setPassword] = React.useState<string>("");
+  const router = useRouter();
 
-  async function onSubmit(event: React.SyntheticEvent) {
+  const onSubmit = async (event: React.SyntheticEvent) => {
     event.preventDefault();
     setIsLoading(true);
 
-    setTimeout(() => {
+    try {
+      const res = await fetch("/api/login", {
+        method: "POST",
+        body: JSON.stringify({ password }),
+      });
+      const { success } = await res.json();
+      if (success) {
+        // router.push("/admin/dashboard");
+        router.refresh();
+      } else {
+        alert("Login failed");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
       setIsLoading(false);
-    }, 3000);
-  }
+    }
+  };
+
+  const handleChangePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
 
   return (
     <div className={cn("grid gap-6", className)} {...props}>
@@ -28,40 +49,26 @@ export function UserAuthForm({ className, ...props }: UserAuthFormProps) {
         <div className="grid gap-2">
           <div className="grid gap-1">
             <Label className="sr-only" htmlFor="email">
-              Email
+              Master password
             </Label>
             <Input
-              id="email"
-              placeholder="name@example.com"
-              type="email"
+              id="password"
+              placeholder="MyLinksHub Key"
+              type="password"
               autoCapitalize="none"
-              autoComplete="email"
               autoCorrect="off"
               disabled={isLoading}
+              onChange={handleChangePassword}
             />
           </div>
           <Button disabled={isLoading}>
             {isLoading && (
               <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
             )}
-            Sign In with Email
+            Unlock
           </Button>
         </div>
       </form>
-      <div className="relative">
-        <div className="absolute inset-0 flex items-center">
-          <span className="w-full border-t" />
-        </div>
-        <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-background px-2 text-muted-foreground">
-            Or continue with
-          </span>
-        </div>
-      </div>
-      <Button variant="outline" type="button" disabled={isLoading}>
-        {isLoading && <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />}{" "}
-        Login
-      </Button>
     </div>
   );
 }
---
+++ src/app/api/login/route.ts
@@ -0,0 +1,34 @@
+// https://nextjs.org/docs/app/building-your-application/routing/route-handlers
+// https://www.makeuseof.com/token-authentication-nextjs-using-jwt/
+import { NextResponse } from "next/server";
+import { SignJWT } from "jose";
+import { getJwtSecretKey } from "@/lib/auth";
+
+export async function POST(request: Request) {
+  const { password } = await request.json();
+
+  if (password !== process.env.ADMIN_MASTER_PASSWORD) {
+    return NextResponse.json(
+      { message: "Unauthorized", error: "AUTH001" },
+      { status: 401 }
+    );
+  }
+
+  const token = await new SignJWT({
+    username: "Admin",
+  })
+    .setProtectedHeader({ alg: "HS256" })
+    .setIssuedAt()
+    .setExpirationTime("30s")
+    .sign(getJwtSecretKey());
+  const response = NextResponse.json(
+    { success: true },
+    { status: 200, headers: { "content-type": "application/json" } }
+  );
+  response.cookies.set({
+    name: "token",
+    value: token,
+    path: "/",
+  });
+  return response;
+}
